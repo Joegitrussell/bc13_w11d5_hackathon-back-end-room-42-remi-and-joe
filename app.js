70b5c9de0a0c749e40b64d84b46ddf86f2d0e939
Joegitrussell <112443220+Joegitrussell@users.noreply.github.com>
2022-12-09 16:04:00 +0000

    set

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from "express";
 import logger from "morgan";
 import cors from "cors";
 import moviesRouter from "./Routes/moviesroutes.js";
-
+const PORT = process.env.PORT;
 const app = express();
 
 app.use(cors());
@@ -10,5 +10,9 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/movies", moviesRouter);
+
+app.listen(PORT, function () {
+   console.log(`Server is listening on http://localhost:${PORT}`);
+ });
 
 export default app;
